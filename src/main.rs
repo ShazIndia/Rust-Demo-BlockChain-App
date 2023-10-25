3cc340e323591241c06dee159405dd0b12caef26
shazindia <shashiranjan@Shashis-MacBook-Pro.local>
2023-10-25 17:45:41 +0530

    adding rocket update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, add_task])
+}
 use std::{fs::{OpenOptions}, io::{Write}};
 use rocket::serde::{Deserialize, json::Json};
 
