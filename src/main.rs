1f9f0e0fbe92a2b9524e59de4cb531a6a7a84960
shazindia <shashiranjan@Shashis-MacBook-Pro.local>
2023-10-25 17:41:06 +0530

    Adding task to web app

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-#[macro_use] extern crate rocket;
+use std::{fs::{OpenOptions}, io::{Write}};
+use rocket::serde::{Deserialize, json::Json};
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    item: &'r str
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[post("/addtask", data="<task>")]
+fn add_task(task: Json<Task<'_>>) -> &'static str {
+    let mut tasks = OpenOptions::new()
+                    .read(true)
+                    .append(true)
+                    .create(true)
+                    .open("tasks.txt")
+                    .expect("unable to access tasks.txt");   
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    tasks.write(task_item_bytes).expect("unable to write to tasks.txt");
+    "Task added succesfully"
 }
