b78630ce82427fa93a162c2e6e53746a26a5a3b6
Shashi Ranjan <shashi11107@gmail.com>
2024-09-04 14:17:52 +0530

    fixing the build issue

--- tests/blockchain_test.rs
+++ tests/blockchain_test.rs
@@ -1,3 +1,6 @@
+// Import the blockchain module from the main crate
+use crate::blockchain::{Block, Blockchain};
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -7,21 +10,17 @@ mod tests {
         let block = Block::new(1, 1627504203, "Test Block".to_string(), "0".to_string());
         assert_eq!(block.index, 1);
         assert_eq!(block.data, "Test Block");
+        assert_eq!(block.previous_hash, "0");
     }
 
     #[test]
-    fn test_blockchain_initialization() {
-        let blockchain = Blockchain::new();
-        assert_eq!(blockchain.chain.len(), 1); // Genesis block
-        assert_eq!(blockchain.chain[0].data, "Genesis Block");
-    }
-
-    #[test]
-    fn test_adding_block() {
+    fn test_blockchain_add_block() {
         let mut blockchain = Blockchain::new();
-        blockchain.add_block("Block 1".to_string());
+        blockchain.add_block("Test Block 1".to_string());
+        blockchain.add_block("Test Block 2".to_string());
 
-        assert_eq!(blockchain.chain.len(), 2);
-        assert_eq!(blockchain.chain[1].data, "Block 1");
+        assert_eq!(blockchain.chain.len(), 3); // Genesis block + 2 added blocks
+        assert_eq!(blockchain.chain[1].data, "Test Block 1");
+        assert_eq!(blockchain.chain[2].data, "Test Block 2");
     }
 }
