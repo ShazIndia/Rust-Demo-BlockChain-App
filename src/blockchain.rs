5043327cf54c086a200c7b50e0159e2156ef7de9
Shashi Ranjan <shashi11107@gmail.com>
2024-09-04 14:29:47 +0530

    fixing build issue

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -53,3 +53,26 @@ impl Blockchain {
         self.chain.push(new_block);
     }
 }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_block_creation() {
+        let block = Block::new(1, 1627504203, "Test Block".to_string(), "0".to_string());
+        assert_eq!(block.index, 1);
+        assert_eq!(block.data, "Test Block");
+        assert_eq!(block.previous_hash, "0");
+    }
+
+    #[test]
+    fn test_blockchain_add_block() {
+        let mut blockchain = Blockchain::new();
+        blockchain.add_block("Test Block 1".to_string());
+        blockchain.add_block("Test Block 2".to_string());
+
+        assert_eq!(blockchain.chain.len(), 3); // Genesis block + 2 added blocks
+        assert_eq!(blockchain.chain[1].data, "Test Block 1");
+        assert_eq!(blockchain.chain[2].data, "Test Block 2");
+    }
+}
