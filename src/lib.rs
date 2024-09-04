1ba98ca78d25c356be5406a631f60ea49ad17c0b
Shashi Ranjan <shashi11107@gmail.com>
2024-09-04 13:54:28 +0530

    Blockchain Application

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+use sha2::{Digest, Sha256};
+use serde::{Serialize, Deserialize};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Block {
+    pub index: u32,
+    pub timestamp: u128,
+    pub data: String,
+    pub previous_hash: String,
+    pub hash: String,
+}
+
+impl Block {
+    pub fn new(index: u32, timestamp: u128, data: String, previous_hash: String) -> Self {
+        let mut hasher = Sha256::new();
+        hasher.update(format!("{}{}{}{}", index, timestamp, data, previous_hash));
+        let result = hasher.finalize();
+        let hash = format!("{:x}", result);
+
+        Block {
+            index,
+            timestamp,
+            data,
+            previous_hash,
+            hash,
+        }
+    }
+}
+
+pub struct Blockchain {
+    pub chain: Vec<Block>,
+}
+
+impl Blockchain {
+    pub fn new() -> Self {
+        Blockchain {
+            chain: vec![Blockchain::create_genesis_block()],
+        }
+    }
+
+    fn create_genesis_block() -> Block {
+        Block::new(0, Blockchain::current_timestamp(), "Genesis Block".to_string(), "0".to_string())
+    }
+
+    pub fn add_block(&mut self, data: String) {
+        let previous_block = self.chain.last().unwrap();
+        let new_block = Block::new(
+            previous_block.index + 1,
+            Blockchain::current_timestamp(),
+            data,
+            previous_block.hash.clone(),
+        );
+        self.chain.push(new_block);
+    }
+
+    fn current_timestamp() -> u128 {
+        SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,11 @@
-pub fn divide_non_zero_result(a: u32, b: u32) -> u32 {
-    if b == 0 {
-        panic!("Divide-by-zero error");
-    } else if a < b {
-        panic!("Divide result is zero");
-    }
-    a / b
-}
+mod blockchain;
+
 fn main() {
+    let mut blockchain = blockchain::Blockchain::new();
+    blockchain.add_block("Block 1 data".to_string());
+    blockchain.add_block("Block 2 data".to_string());
 
-    divide_non_zero_result(10,5);
-
-}
-
-
-/// Using hidden `try_main` in doc tests.
-///
-/// ```
-/// # // hidden lines start with `#` symbol, but they're still compilable!
-/// # fn try_main() -> Result<(), String> { // line that wraps the body shown in doc
-/// let res = doccomments::try_div(10, 2)?;
-/// # Ok(()) // returning from try_main
-/// # }
-/// # fn main() { // starting main that'll unwrap()
-/// #    try_main().unwrap(); // calling try_main and unwrapping
-/// #                         // so that test will panic in case of error
-/// # }
-/// ```
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_divide() {
-        assert_eq!(divide_non_zero_result(10, 2), 5);
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_any_panic() {
-        divide_non_zero_result(1, 0);
-    }
-
-    #[test]
-    #[should_panic(expected = "Divide result is zero")]
-    fn test_specific_panic() {
-        divide_non_zero_result(1, 10);
+    for block in blockchain.chain.iter() {
+        println!("{:?}", block);
     }
 }
